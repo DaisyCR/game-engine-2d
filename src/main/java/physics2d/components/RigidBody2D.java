543a60da99f3ecd7650eb3e6d733a72a93a98ba0
Daisy Reis <90834386+DaisyCR@users.noreply.github.com>
2023-08-22 17:50:35 -0300

    Added integration with JBox2D

---
+++ src/main/java/physics2d/components/RigidBody2D.java
@@ -0,0 +1,92 @@
+package physics2d.components;
+
+import components.Component;
+import org.jbox2d.dynamics.Body;
+import org.joml.Vector2f;
+import physics2d.enums.BodyType;
+
+public class RigidBody2D extends Component {
+    private Vector2f velocity = new Vector2f();
+    private float angularDamping = 0.8f;
+    private float linearDamping = 0.9f;
+    private float mass = 0.0f;
+    private BodyType bodyType = BodyType.Dynamic;
+    private boolean fixedRotation = false;
+    private boolean continuousCollision = true;
+    private Body rawbody = null;
+
+    @Override
+    public void update(float deltaTime){
+        if(rawbody != null){
+            this.gameObject.transform.position.set(
+                    rawbody.getPosition().x,
+                    rawbody.getPosition().y
+            );
+            this.gameObject.transform.rotation = (float) Math.toDegrees(rawbody.getAngle());
+        }
+    }
+
+    public Vector2f getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(Vector2f velocity) {
+        this.velocity = velocity;
+    }
+
+    public float getAngularDamping() {
+        return angularDamping;
+    }
+
+    public void setAngularDamping(float angularDamping) {
+        this.angularDamping = angularDamping;
+    }
+
+    public float getLinearDamping() {
+        return linearDamping;
+    }
+
+    public void setLinearDamping(float linearDamping) {
+        this.linearDamping = linearDamping;
+    }
+
+    public float getMass() {
+        return mass;
+    }
+
+    public void setMass(float mass) {
+        this.mass = mass;
+    }
+
+    public BodyType getBodyType() {
+        return bodyType;
+    }
+
+    public void setBodyType(BodyType bodyType) {
+        this.bodyType = bodyType;
+    }
+
+    public boolean isFixedRotation() {
+        return fixedRotation;
+    }
+
+    public void setFixedRotation(boolean fixedRotation) {
+        this.fixedRotation = fixedRotation;
+    }
+
+    public boolean isContinuousCollision() {
+        return continuousCollision;
+    }
+
+    public void setContinuousCollision(boolean continuousCollision) {
+        this.continuousCollision = continuousCollision;
+    }
+
+    public Body getRawbody() {
+        return rawbody;
+    }
+
+    public void setRawbody(Body rawbody) {
+        this.rawbody = rawbody;
+    }
+}
